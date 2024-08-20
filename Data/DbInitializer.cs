433a0c9f4474c6a052e967cdff7c233a829504cc
qasas145 <mohamed.sayed14527@yahoo.com>
2024-08-20 09:18:34 +0300

    Senstive data when initializing has been hided

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -29,11 +29,11 @@ public class DbInitializer : IDbInitializer {
             _roleManager.CreateAsync(new IdentityRole("Employee")).GetAwaiter().GetResult();
             
             var res = _userManager.CreateAsync(new ApplicationUser{
-                UserName = "mohamed.sayed14527@yahoo.com",
-                Email = "mohamed.sayed14527@yahoo.com",
+                UserName = "mohamed@yahoo.com",
+                Email = "mohamed@yahoo.com",
                 PhoneNumber = "01708119559",
                 Name = "Muhammad elsayed "
-            }, "Hamada1020$").GetAwaiter().GetResult();
+            }, "Admin1234@").GetAwaiter().GetResult();
             foreach (var item in res.Errors)
             {
                 Console.WriteLine(item.Description);
